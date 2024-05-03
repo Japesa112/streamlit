e990ec18e2174c3c9a4e8881fe36a01fdd1d0c75
Japesa112 <130290441+Japesa112@users.noreply.github.com>
2024-05-03 23:00:25 +0300

    Update audio-transcriber.py

--- audio-transcriber.py
+++ audio-transcriber.py
@@ -5,20 +5,15 @@ st.title("Podcast Transcriber")
 
 st.sidebar.header("Transcription section")
 
-audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
+#audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
 
 process_button = st.sidebar.button("Transcribe Episode")
 st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patient.")
 
 model = whisper.load_model("base")
 st.text("Whisper Model Loaded")
-
-if process_button:
-    if audio_file is not None:
-        st.sidebar.info("Transcribing audio...")        
-        result = model.transcribe(audio_file.name)
+st.sidebar.info("Transcribing audio...")        
+result = model.transcribe("https://firebasestorage.googleapis.com/v0/b/daily-devotion-72b13.appspot.com/o/devotion_preaching%2Foutput2.mp3?alt=media&token=3a035a32-8475-4357-9ad8-815d7981240f")
         
-        st.sidebar.success("Transcription Complete")
-        st.markdown(result['text'])
-    else:
-        st.sidebar.error("Please upload an episode.")
+st.sidebar.success("Transcription Complete")
+st.markdown(result['text'])
