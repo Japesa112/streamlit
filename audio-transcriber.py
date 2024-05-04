ff5f6d2732a3cc197d9139ffc224fb7e5eef993f
Japesa112 <130290441+Japesa112@users.noreply.github.com>
2024-05-04 09:00:17 +0300

    Update audio-transcriber.py

--- audio-transcriber.py
+++ audio-transcriber.py
@@ -13,7 +13,7 @@ st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patie
 model = whisper.load_model("base")
 st.text("Whisper Model Loaded")
 st.sidebar.info("Transcribing audio...")        
-result = model.transcribe("https://firebasestorage.googleapis.com/v0/b/daily-devotion-72b13.appspot.com/o/devotion_preaching%2FDEVOTION%20FOR%20FRIDAY%2026TH%20APRIL%202024%20WITH%20FR%20EUSTACE%20SIAME%20SDB!.mp4?alt=media&token=b5e28a04-5e5f-45d4-b864-a12f9c20c81a")
+result = model.transcribe("https://firebasestorage.googleapis.com/v0/b/daily-devotion-72b13.appspot.com/o/devotion_preaching%2Foutput4.mp3?alt=media&token=f68e59d2-eecc-4b2b-85f1-6e4eb44f707b")
         
 st.sidebar.success("Transcription Complete")
 st.markdown(result['text'])
