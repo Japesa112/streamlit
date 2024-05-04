c5e0b61a2a772ffe186607027bb6fd5ade544551
Japesa112 <130290441+Japesa112@users.noreply.github.com>
2024-05-04 11:33:20 +0300

    Update audio-transcriber.py

--- audio-transcriber.py
+++ audio-transcriber.py
@@ -1,19 +1,24 @@
 import streamlit as st
 import whisper 
+import json
 
 st.title("Podcast Transcriber")
 
 st.sidebar.header("Transcription section")
 
-#audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
+url_input = st.sidebar.text_input("Enter audio URL")
 
 process_button = st.sidebar.button("Transcribe Episode")
 st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patient.")
 
 model = whisper.load_model("base")
 st.text("Whisper Model Loaded")
-st.sidebar.info("Transcribing audio...")        
-result = model.transcribe("https://firebasestorage.googleapis.com/v0/b/daily-devotion-72b13.appspot.com/o/devotion_preaching%2Foutput2.mp3?alt=media&token=3a035a32-8475-4357-9ad8-815d7981240f")
-        
-st.sidebar.success("Transcription Complete")
-st.markdown(result['text'])
+
+if process_button and url_input:
+    st.sidebar.info("Transcribing audio...")        
+    result = model.transcribe(url_input)
+    st.sidebar.success("Transcription Complete")
+    
+    # Convert the result to JSON format
+    json_result = json.dumps(result)
+    st.json(json_result)
